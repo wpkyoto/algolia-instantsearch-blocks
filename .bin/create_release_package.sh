9676e4f8e3657100d2387ab7db2c54077f7159ca
Hidetaka Okamoto <info@wp-kyoto.net>
2020-12-03 13:20:18 +0900

    chore: prepare release

--- .bin/create_release_package.sh
+++ .bin/create_release_package.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 set -eo
 npm install
+rm -rf node_modules/@types/algoliasearch-helper
 npm run build
 wget https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/reset-min.css -P build  
 wget https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/algolia-min.css -P build  
